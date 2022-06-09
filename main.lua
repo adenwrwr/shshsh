ade7caed51937387de5fe28f599d6dbeffbcd6cc
adenwrwr <100206873+adenwrwr@users.noreply.github.com>
2022-06-08 20:55:12 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,42 @@
+--Beemo's simple whitelist
+_G.Key = "corgs"
+
+local keys = {
+    "corgs",
+    "yelp",
+    "bark"
+    "chew",
+    "strech",
+    "lick"
+}
+
+local counter = 1
+local keyCheck
+for i,v in pairs(keys) do
+    if counter == #keys then
+    --not whitelisted!
+    keys = ""
+    game.Players.LocalPlayer:Kick("Not a valid key!")
+    else
+        if v == _G.Key then
+            --Whitelisted!
+            print("Successfully whitelisted!")
+            keyCheck = _G.Key
+            keys = ""
+        else
+            counter = counter +1
+        end
+    end
+end
+
+while true do
+    if _G.Key == keyCheck then
+        --Not spoofed
+    else
+        game.Players.LocalPlayer:Kick("Do not try and spoof your key!")
+    end
+    wait()
+end
 local MoveMouse = mousemoverel
 local ReadFile = readfile
 local WriteFile = writefile
